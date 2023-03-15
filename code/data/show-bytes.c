d385a0430553214c8e4addb4fb802e5abbb449d9
Yadong Zhang <zhyd007@gmail.com>
2023-03-15 07:41:53 +0000

    extending bits.

--- code/data/show-bytes.c
+++ code/data/show-bytes.c
@@ -39,6 +39,26 @@ void test_show_bytes(int val) {
     show_short(minus_sval);
 }
 
+/**
+ * runs as 32-bit program by passing -m32 option
+**/
+void extend_bits(int val) { // pass 12345 as parameter
+    short sx = -val;
+    unsigned short usx = sx;
+    int x = sx;
+    unsigned ux = usx;
+
+    printf("sx = %d:\t", sx);
+    show_bytes((byte_pointer) &sx, sizeof(short));
+    printf("usx = %u:\t", usx);
+    show_bytes((byte_pointer) &usx, sizeof(unsigned short));
+    printf("x   = %d:\t", x);
+    show_bytes((byte_pointer) &x, sizeof(int));
+    printf("ux  = %u:\t", ux);
+    show_bytes((byte_pointer) &ux, sizeof(unsigned));
+}
+
 int main() {
     test_show_bytes(12345);
+    extend_bits(12345);
 }
